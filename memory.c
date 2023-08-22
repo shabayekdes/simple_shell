812248e2bfe08710a7b6bb96894e9bb3cad39bad
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-08-22 19:54:31 +0300

    Add safely_free_pointer method

--- memory.c
+++ memory.c
@@ -14,3 +14,19 @@ void free_strings(char **pp)
 		free(*pp++);
 	free(a);
 }
+/**
+ * safely_free_pointer - Frees memory allocated for a pointer
+ * @ptr: address of pointer to be free
+ *
+ * Return: 1 if freed, or 0.
+ */
+int safely_free_pointer(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
