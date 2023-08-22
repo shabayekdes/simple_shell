b8c1854cd5f18ac45f380f62b8c859fb127b3a8f
Amal Ragheb <je.amal985@gmail.com>
2023-08-22 08:11:09 -0400

    add func 14

---
+++ utilties.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * _atoi - converts string to integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string or -1 on error or converted number
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	unsigned long int result = 0;
+
+	if (*s == '+')
+		s++;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+			if (result > INT_MAX)
+				return (-1);
+		}
+		else
+			return (-1);
+	}
+	return (result);
+}
+
