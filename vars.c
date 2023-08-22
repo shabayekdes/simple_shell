525deb42d776eeff95d1a262b4a347c831e7a8d6
Amal Ragheb <je.amal985@gmail.com>
2023-08-22 08:18:31 -0400

    add func 19

---
+++ vars.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+int replace_string(char **old, char *new);
+/**
+ * detect_chain_delimiter - test if current char in buffer is a chain delimeter
+ * @data: parameter struct
+ * @buf: char buffer
+ * @p: address of current position in buf
+ * Return: 1 if chain delimeter or 0
+ */
+int detect_chain_delimiter(esh_t *data, char *buf, size_t *p)
+{
+	size_t j = *p;
+
+	if (buf[j] == '|' && buf[j + 1] == '|')
+	{
+		buf[j] = 0;
+		j++;
+		data->buffer_type = 1;
+	}
+	else if (buf[j] == '&' && buf[j + 1] == '&')
+	{
+		buf[j] = 0;
+		j++;
+		data->buffer_type = 2;
+	}
+	else if (buf[j] == ';')
+	{
+		buf[j] = 0;
+		data->buffer_type = 3;
+	}
+	else
+		return (0);
+	*p = j;
+	return (1);
+}
+
