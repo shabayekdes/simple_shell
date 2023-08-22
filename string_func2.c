92494613be7b966f6db15b4471eb278bc5f37209
Amal Ragheb <je.amal985@gmail.com>
2023-08-22 07:58:24 -0400

    add func 9

--- string_func2.c
+++ string_func2.c
@@ -16,3 +16,21 @@ char *_strchr(char *s, char c)
 	return (NULL);
 }
 
+/**
+ * _strcat - concatenates two strings
+ * @dest: the destination buffer
+ * @src: the source buffer
+ * Return: pointer to destination buffer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
+
