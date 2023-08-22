b208308a2aecbca9fe4c0f96d0d5e6c483b1bf9b
Esmail Shabayek <esmail.shabayek@gmail.com>
2023-08-22 19:49:51 +0300

    Add execute_command method

--- execute.c
+++ execute.c
@@ -39,3 +39,50 @@ char *find_path(char *path_str, char *command)
 	}
 	return (NULL);
 }
+
+/**
+ * execute_command - execute a command in PATH
+ * @data: parameter and return struct
+ * Return: void
+ */
+void execute_command(esh_t *data)
+{
+	char *path = NULL;
+	int i, k;
+	pid_t child_pid;
+
+	data->path = data->argv[0];
+	if (data->flag == 1)
+		data->line_count++;
+	data->flag = 0;
+
+	for (i = 0, k = 0; data->arg[i]; i++)
+		if (!is_delimiter(data->arg[i], " \t\n"))
+			k++;
+	if (!k)
+		return;
+	path = find_path(_getenv(data, "PATH="), data->argv[0]);
+	if (path)
+		data->path = path;
+
+	child_pid = fork();
+	if (child_pid == -1)
+		perror("Error:");
+	return;
+	if (child_pid == 0)
+	{
+		if (execve(data->path, data->argv, get_environ(data)) == -1)
+			reset_esh(data);
+			if (errno == EACCES)
+				exit(126);
+		exit(1);
+	}
+	else
+	{
+		wait(&(data->status));
+		if (WIFEXITED(data->status))
+			data->status = WEXITSTATUS(data->status);
+			if (data->status == 126)
+				print_error(data, "Permission denied\n");
+	}
+}
